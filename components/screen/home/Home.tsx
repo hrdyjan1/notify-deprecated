2c74f0c5d5aea1189c9c3aefe89f9c1b7e0d2cdb
Jan Hrdý <hrdy@czc.cz>
2021-07-21 20:57:44 +0200

    Initialized Reanimated 2

--- components/screen/home/Home.tsx
+++ components/screen/home/Home.tsx
@@ -1,10 +1,46 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import Animated, {
+  useSharedValue,
+  withTiming,
+  useAnimatedStyle,
+  Easing,
+} from 'react-native-reanimated';
+import {View, Button} from 'react-native';
 
 function Home() {
+  const randomWidth = useSharedValue(10);
+
+  const config = {
+    duration: 500,
+    easing: Easing.bezier(0.5, 0.01, 0, 1),
+  };
+
+  const style = useAnimatedStyle(() => {
+    return {
+      width: withTiming(randomWidth.value, config),
+    };
+  });
+
   return (
-    <View>
-      <Text>First page</Text>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+      }}>
+      <Animated.View
+        style={[
+          {width: 100, height: 80, backgroundColor: 'black', margin: 30},
+          style,
+        ]}
+      />
+      <Button
+        title="toggle"
+        onPress={() => {
+          randomWidth.value = Math.random() * 350;
+        }}
+      />
     </View>
   );
 }
